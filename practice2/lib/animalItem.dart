6223245cf7e16e9056b4437bba252a75ef9a49d6
최혜미 <ghrltjdtprbs@gmail.com>
2023-08-05 22:20:47 +0900

    Add files via upload

---
+++ practice2/lib/animalItem.dart
@@ -0,0 +1,15 @@
+class Animal{
+  String? imagePath;
+  String? animalName;
+  String? kind;
+  bool? flyExist = false;
+
+  Animal(
+  {
+    required this.animalName,
+    required this.imagePath,
+    required this.kind,
+    this.flyExist
+}
+      );
+}
