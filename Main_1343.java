ace188f34cac81b92f2366bc8b10150635510604
최혜미 <ghrltjdtprbs@gmail.com>
2023-08-26 23:48:09 +0900

    Add files via upload

---
+++ Main_1343.java
@@ -0,0 +1,33 @@
+package bak;
+
+import java.util.*;
+
+public class Main_1343 {
+ 
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+ 
+        String res = "";
+ 
+        res = poliomino(s);
+ 
+        System.out.println(res);
+    }
+ 
+    private static String poliomino(String s) {
+        String ans = "";
+        String A = "AAAA", B = "BB";
+        
+        s = s.replaceAll("XXXX", A);
+        ans = s.replaceAll("XX", B);
+        
+        if(ans.contains("X")) {
+            ans = "-1";
+        }
+ 
+        return ans;
+    }
+}
+
