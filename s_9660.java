5244c85623a8e9df921a5fc826c2fd008470905a
최혜미 <ghrltjdtprbs@gmail.com>
2024-10-21 19:04:36 +0900

    Add files via upload

---
+++ s_9660.java
@@ -0,0 +1,14 @@
+package softeer;
+
+public class s_9660 {
+    public static void main(String args[]){
+        System.out.println("##  ##     ##     #####    #####    ##  ##            ##  ##            ##   ##    ##      ####");
+        System.out.println("##  ##    ####    ##  ##   ##  ##   ##  ##            ##  ##            ### ###   ####    ##  ##");
+        System.out.println("##  ##   ##  ##   ##  ##   ##  ##   ##  ##             ####             #######  ##  ##   ##");
+        System.out.println("######   ######   #####    #####     ####               ##     ######   ## # ##  ######    ####");
+        System.out.println("##  ##   ##  ##   ##       ##         ##               ####             ##   ##  ##  ##       ##");
+        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##   ##  ##");
+        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##    ####");
+    }
+
+}
